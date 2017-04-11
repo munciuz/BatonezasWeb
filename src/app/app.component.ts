9c3c08850e1a946b43bcce7909134e28f6999186
munciuz <mantas.dubickas@ktu.edu>
2017-04-11 13:17:05 +0300

    finally working angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'batonezas-app',
+    moduleId: module.id,
+    templateUrl: './app.component.html'
+})
+export class AppComponent { }
